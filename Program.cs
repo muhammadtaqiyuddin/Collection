5f07faeede575f03f4845ca4faa80663b401feaf
mtaqiyuddin <mtaqiyuddin101@gmail.com>
2022-07-10 14:12:46 +0700

    collection

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LatihanCollection;
+class Program{
+    static void Main(string[] args)
+    {
+        // membuat objek list untuk menampung tipe data integer
+        List<int> listNilai = new List<int>();
+
+        // menambahkan element list
+        // tidak ada proses boxing
+        listNilai.Add(70);
+        listNilai.Add(100);
+        listNilai.Add(85);
+
+        // mengakses element list
+        // tidak ada proses unboxing
+        int nilaiElement1 = listNilai[1];
+
+        Console.WriteLine(nilaiElement1);
+        Console.WriteLine();
+
+        foreach (int nilai in listNilai)
+        {
+            Console.WriteLine(nilai);   
+        }
+        Console.ReadKey();  
+
+    }
+}
